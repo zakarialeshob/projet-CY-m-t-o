05762c208ba115102c1bfc03ef1b61a1a0adc3b2
zakarialeshob <106832108+zakarialeshob@users.noreply.github.com>
2023-02-04 00:52:06 +0100

    Update t1.sh

--- t1.sh
+++ t1.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/gnuplot -persist  
 set title "Temperature" 
-set ylabel "Moyenne Temperature" 
-set xlabel "identifiant de la station" 
-plot 't1.csv' using 1:2:3 with title "Minimum et maximum", '' using 1:3 with lines title "Moyenne"
+set ylabel "Median Temperature" 
+set xlabel "station id" 
+plot 't1.csv' using 1:2:3 with title "Min and max", '' using 1:3 with lines title "Moyenne"
