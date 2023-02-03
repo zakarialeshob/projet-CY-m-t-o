0711c3c5907782c7e964846858073043f6b590ca
zakarialeshob <106832108+zakarialeshob@users.noreply.github.com>
2023-02-04 00:20:54 +0100

    Update mainm.c

--- mainm.c
+++ mainm.c
@@ -3,13 +3,13 @@
 #include <string.h>
 #include "functm.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[]) {		
 	int i, j, nb=0;
 	float col1, col2, col3, col4;
 	char* filename1=argv[1];
 	char* filename2=argv[2];
 	char* option=argv[3];
-	FILE *file = fopen(filename1, "r");
+	FILE *file = fopen(filename1, "r");		//verification for the file
 	if (file == NULL) {
 		return 2;
 	}
@@ -21,26 +21,26 @@ int main(int argc, char* argv[]) {
 	Node* nliste = NULL;
 	Node* tg=NULL;
 	char line[1024];
-	while(fgets(line, 1024, file) != NULL){
+	while(fgets(line, 1024, file) != NULL){		//compt the number of line
 		nb++;
 	}
 	printf("%d\n", nb);
 	fclose(file);
 	file=fopen(filename1, "r");
-	for (i=1; i<=100000; i++){
-		fscanf(file, "%f,%f,%f,%f", &col1, &col2, &col3, &col4);
+	for (i=1; i<=100000; i++){		
+		fscanf(file, "%f,%f,%f,%f", &col1, &col2, &col3, &col4);		//create the list node 
 		liste = addEnd(liste, col1,col2,col3,col4); 
 	}  
 	tg=liste;
 	fclose(file);
-	if(option == "r"){
+	if(option == "r"){			// sort the list with "r" or not
 		nliste = sortNodeR(tg);
 	}
 	else{
 		nliste = sortNode(tg);
 	}
 	while (nliste->next!=NULL){
-		fprintf(output,"%f,%f,%f,%f\n", nliste->id,nliste->x,nliste->y,nliste->moisture);
+		fprintf(output,"%f,%f,%f,%f\n", nliste->id,nliste->x,nliste->y,nliste->moisture);	//replace the data in new file
 		nliste=nliste->next;
 	}
 	fclose(output);
